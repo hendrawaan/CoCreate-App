29a8acbf86721d842216449c144e8841d0b21e17
zeastank.harits <zeastank.harits@gmail.com>
2020-11-11 15:36:35 +0700

    init_branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Halaman Home
         </a>
       </header>
     </div>
