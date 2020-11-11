e748b5e34735c65d5b56e3f21d9069e0214cdb88
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-11 23:31:54 +0800

    Fixed typo.

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,11 +1,10 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { create } from "react-test-renderer";
-import { Login } from "../pages";
 
 describe("Test halaman login", () => {
   test("Harus sesuai dengan snapshot komponen heading", () => {
-    const heading = create(<Login />);
-    expect(heading.toJSON()).toMatchSnapshot();
+    const Login = create(<Login />);
+    expect(Login.toJSON()).toMatchSnapshot();
   });
 });
