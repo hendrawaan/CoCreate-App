bb768f5082fd04d592ede7151e9d161db4f32bac
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-11 22:15:56 +0800

    First Component.

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import loginIllustrator from "../assets/img/login-illustrator.svg";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <Container>
+      <Row className="vh-100 align-items-center row-login">
+        <Col md={8} lg={9} className="d-none d-md-block">
+          <img
+            className="w-100"
+            src={loginIllustrator}
+            alt="Login Illustration"
+          />
+        </Col>
+        <Col md={4} lg={3}>
+          <h1 className="mb-5">Login</h1>
+          <p className="mb-4">
+            Hai Selemat datang!<br />Login dulu yuk...
+          </p>
+          <Form>
+            <Form.Group controlId="formEmail">
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <p>
+              Lupa password? <a href="./">Klik disini</a>
+            </p>
+            <Button className="mt-5 mb-3" variant="primary" type="submit" block>
+              Login
+            </Button>
+            <p>
+              Belum punya akun? <a href="./">Klik disini</a>
+            </p>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
