27d18baf148f5e7c3f036c4b93acb187c69e982b
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-12 00:32:08 +0800

    Add docs.

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,10 +1,18 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { create } from "react-test-renderer";
+import { Login } from "../pages";
 
-describe("Test halaman login", () => {
-  test("Harus sesuai dengan snapshot komponen heading", () => {
-    const Login = create(<Login />);
-    expect(Login.toJSON()).toMatchSnapshot();
+/**
+ * Test untuk halaman login.
+ */
+describe("Test Halaman Login", () => {
+  /**
+   * Memastikan halaman login memiliki kecocokan dengan snapshoot
+   * Login.test.js.snap.
+   */
+  test("Halaman Login Harus sesuai dengan snapshot komponen login", () => {
+    const login = create(<Login />);
+    expect(login.toJSON()).toMatchSnapshot();
   });
 });
