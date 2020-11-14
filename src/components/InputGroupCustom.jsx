3dda3354d4089d98d04675d2453641bb678c6ec5
hendrawaan <hendrawanarif07@gmail.com>
2020-11-14 16:37:38 +0700

    updated disabled feature

--- src/components/InputGroupCustom.jsx
+++ src/components/InputGroupCustom.jsx
@@ -3,13 +3,20 @@ import {
     InputGroup
 } from "react-bootstrap";
 
-const InputGroupCustom = (props) => {
-    return (<InputGroup >
-        <InputGroup.Prepend>
-            <InputGroup.Text>{props.icon}</InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
-    </InputGroup>
+const InputGroupCustom = ({ disabled, ...props }) => {
+    let form_control = ""
+    if (disabled) {
+        form_control = <FormControl disabled onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
+    } else {
+        form_control = <FormControl onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
+    }
+    return (
+        <InputGroup >
+            <InputGroup.Prepend>
+                <InputGroup.Text>{props.icon}</InputGroup.Text>
+            </InputGroup.Prepend>
+            {form_control}
+        </InputGroup>
     )
 }
 export default InputGroupCustom
