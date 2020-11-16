ddd9f2d38d7d160217f5fa35a1d22d7c500920e9
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-16 23:01:59 +0800

    Remove url.

--- src/api/api-list.js
+++ src/api/api-list.js
@@ -1,10 +1,11 @@
-const API_URL = "http://54.158.203.226:80";
-
 // Login page.
-export const LOGIN = `${API_URL}/api/v1/login`;
+export const LOGIN = `/api/v1/login`;
 
 // Mendaftarkan user baru.
-export const ADD_USER = `${API_URL}/api/v1/user/add`;
+export const ADD_USER = `/api/v1/user/add`;
+
+// Mendapatkan profile user
+export const GET_PROFILE = `/api/v1/user/detail`
 
 // Menampilkan semua daftar user.
-export const LIST_USERS = `${API_URL}/api/v1/users`;
+export const LIST_USERS = `/api/v1/users`;
