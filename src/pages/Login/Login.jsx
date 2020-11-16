72216f1781c7253d4e7478f947fb06e463f33c83
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-16 12:56:20 +0800

    Add some methode login, and change code to using redux.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Button, Col, Container, Form, Row, Spinner } from "react-bootstrap";
-import { login } from "../../actions/user";
+import { FaEnvelope } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 import { loginIllustration, logoSmall } from "../../assets/images";
+import { FacebookLoginBtn, GoogleLoginBtn } from "../../components";
+import { login } from "../../store/user";
 import "./Login.css";
 
 /**
@@ -11,43 +14,36 @@ import "./Login.css";
  * Date   : 11/11/2020
  */
 const Login = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [actionLogin, setActionLogin] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
+  const dispatch = useDispatch();
+  const { error, loading } = useSelector((state) => state.user);
+
+  const [dataLogin, setDataLogin] = useState();
+  const updateField = (e) => {
+    setDataLogin({
+      ...dataLogin,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // TODO: Nanti akan diimplementasikan jika backend bisa menghandle
+  // login via FB
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+
+  // TODO: Nanti akan diimplementasikan jika backend bisa menghandle
+  // login via Google
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
 
   /**
-   * Fungsi untuk menangani proses ketika user mensubmit permintaan login.
+   * Fungsi untuk menangani submit form login.
    * @param {object} e Element
    */
   const submitHandler = (e) => {
     e.preventDefault();
-    setErrorMsg();
-    setActionLogin(true);
-
-    login({
-      name: name,
-      password: password,
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        const { code, data } = res;
-        if (code === 200) {
-          localStorage.setItem("token", data.token);
-          window.location = "/";
-        } else if (code === 400) {
-          setErrorMsg("Nama atau password salah");
-        } else {
-          throw new Error("Error");
-        }
-      })
-      .catch((err) => {
-        setErrorMsg("Upss... Sepertinya terjadi kesalahan.");
-        console.log(err);
-      })
-      .finally(() => {
-        setActionLogin(false);
-      });
+    dispatch(login(dataLogin));
   };
 
   return (
@@ -61,9 +57,8 @@ const Login = () => {
           />
         </Col>
         <Col lg={3}>
-          <img className="mx-auto d-block mb-4" src={logoSmall} alt="logo" />
+          <img className="mx-auto d-block mb-3" src={logoSmall} alt="logo" />
           <h1 className="mb-4 text-center">Login</h1>
-          <hr />
           <Form onSubmit={submitHandler}>
             <Form.Group controlId="name">
               <Form.Control
@@ -72,7 +67,7 @@ const Login = () => {
                 placeholder="Masukkan nama/email"
                 name="name"
                 autoComplete="username"
-                onChange={(e) => setName(e.target.value)}
+                onChange={updateField}
               />
             </Form.Group>
             <Form.Group controlId="password">
@@ -82,35 +77,42 @@ const Login = () => {
                 placeholder="Password"
                 name="password"
                 autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={updateField}
               />
             </Form.Group>
-            <p>
-              Lupa password? <a href="./">Klik disini</a>
-            </p>
-            <small className="text-danger ">{errorMsg}</small>
+            <small>
+              Lupa password? <a href="./">Klik disini</a> <br />
+              <span className="text-danger ">{error}</span>
+            </small>
+
             <Button
-              className="mt-4 mb-3"
-              variant="primary"
+              className="my-3"
+              variant="outline-secondary"
               type="submit"
               block
-              disabled={actionLogin}
+              disabled={loading}
             >
-              {actionLogin && (
+              {loading ? (
                 <Spinner
                   as="span"
                   animation="border"
                   size="sm"
                   role="status"
+                  className="mr-2"
                   aria-hidden="true"
                 />
+              ) : (
+                <FaEnvelope className="mr-2" />
               )}
-              <span className="ml-2">Masuk</span>
+              <span>Masuk dengan Email</span>
             </Button>
-            <p>
-              Belum punya akun? <a href="./">Klik disini</a>
-            </p>
           </Form>
+
+          <div className="hr-text">
+            <span>atau</span>
+          </div>
+          <FacebookLoginBtn callback={responseFacebook} />
+          <GoogleLoginBtn callback={responseGoogle} />
         </Col>
       </Row>
     </Container>
