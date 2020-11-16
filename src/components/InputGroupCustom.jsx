1bdbde6a1f50a3def5e54d3dcd7f9c7feda63a32
hendrawaan <hendrawanarif07@gmail.com>
2020-11-16 12:21:05 +0700

    perbaikan inputgroup, penambahan select dan as

--- src/components/InputGroupCustom.jsx
+++ src/components/InputGroupCustom.jsx
@@ -3,12 +3,19 @@ import {
     InputGroup
 } from "react-bootstrap";
 
-const InputGroupCustom = ({ disabled, ...props }) => {
+const InputGroupCustom = ({ children, disabled, ...props }) => {
     let form_control = ""
     if (disabled) {
-        form_control = <FormControl disabled onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
+        form_control = <FormControl disabled as={props.as} defaultValue={props.defValue} onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
     } else {
-        form_control = <FormControl onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} />
+        form_control = <FormControl as={props.as} onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} defaultValue={props.defValue} />
+    }
+    if (props.as === "select") {
+        form_control = <FormControl as={props.as} onChange={(e) => props.setFunc(e.target.value)} type={props.type} placeholder={props.placeholder} >
+            {children.map(function (item, i) {
+                return (<option key={i} value={item.key}>{item.name}</option>)
+            })}
+        </FormControl >;
     }
     return (
         <InputGroup >
