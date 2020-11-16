04ea55c252250a63d4d229a34142cb117cd27513
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-16 12:50:46 +0800

    Add some API endpoint.

---
+++ src/api/api-list.js
@@ -0,0 +1,10 @@
+const API_URL = "http://54.158.203.226:80";
+
+// Login page.
+export const LOGIN = `${API_URL}/api/v1/login`;
+
+// Mendaftarkan user baru.
+export const ADD_USER = `${API_URL}/api/v1/user/add`;
+
+// Menampilkan semua daftar user.
+export const LIST_USERS = `${API_URL}/api/v1/users`;
