d55b2032657674e27dace5b6eb94c69818f204ad
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-17 23:59:21 +0800

    Add notFoundIlustration.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,10 @@
-import loginIllustration from './illustration/login-illustration.svg'
-import logoSmall from './logo/logo-small.png'
+import loginIllustration from "./illustration/login-illustration.svg";
+import notFoundIlustration from "./illustration/not-found-illustration.svg";
+import logoSmall from "./logo/logo-small.png";
 
 export {
   loginIllustration,
-  logoSmall
-}
+  logoSmall,
+  notFoundIlustration
+};
 
