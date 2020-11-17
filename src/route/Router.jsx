9ed5051d1ab0f0b4fc37ad963a069f0cf04bcc7b
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-18 00:01:12 +0800

    Add Admin and not found page.

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { NavApp } from "../components";
-import { Home, Login, Profile, Trending } from "../pages";
-import ProtectedRoute from "./ProtectedRoute";
+import { Admin, Home, Login, NotFound, Profile, Trending } from "../pages";
+import { ProtectedRoute } from "./ProtectedRoute";
 
 const Router = () => {
   const navItems = [
-    { label: "Home",     link: "/",         isProtected: false },
-    { label: "Profile",  link: "/profile",  isProtected: true },
+    { label: "Home", link: "/", isProtected: false },
+    { label: "Profile", link: "/profile", isProtected: true },
     { label: "Trending", link: "/trending", isProtected: true },
   ];
 
@@ -19,6 +19,8 @@ const Router = () => {
         <Route exact path="/login" component={Login} />
         <ProtectedRoute exact path="/profile" component={Profile} />
         <ProtectedRoute exact path="/trending" component={Trending} />
+        <ProtectedRoute exact path="/admin" component={Admin} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
