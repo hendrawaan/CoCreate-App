e13af77e8c9bed8698890289bce23f6ed5bb9ff9
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-18 00:01:34 +0800

    Add admin and not found page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+export * from "./Admin/Admin";
 export * from "./Home/Home";
 export * from "./Login/Login";
+export * from "./NotFound/NotFound";
 export * from "./Profile/Profile";
 export * from "./Trending/Trending";
 
