db077aa23d0d119cddc9334b035804b1886eb8d0
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-18 00:03:54 +0800

    Remove login/logout button.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
-import { Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getProfile } from "../../store/profile";
-import { logout } from "../../store/user";
 
 /* Hanya untuk testing */
-export default function Home() {
+export function Home() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
   const { profile } = useSelector((state) => state.profile);
@@ -18,16 +16,6 @@ export default function Home() {
   return (
     <div className="text-center m-5">
       <h1>Ini Halaman Home</h1>
-      <br />
-      <Button
-        variant="primary"
-        type="submit"
-        onClick={
-          user ? () => dispatch(logout()) : () => (window.location = "/login")
-        }
-      >
-        {user ? "Logout" : "Login"}
-      </Button>
       <br />
       {profile && (
         <div className="mt-5">
