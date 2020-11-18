e41d92e723b9e840151807ddde69817ad970a5ba
Muhamad Hilman <hilmanm332@gmail.com>
2020-11-18 13:13:09 +0700

    typo

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 import App from "./App";
-// import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'mdbreact/dist/css/mdb.css';
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AdminHome from './pages/PagesAdmin/AdminHome';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
->>>>>>> hilman
 
 ReactDOM.render(
   <Provider store={store}>
