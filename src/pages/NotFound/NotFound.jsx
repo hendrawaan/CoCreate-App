3f5e647983c43da79d81573c9d4de1fb9ca44451
febrianblo <febrianblo@gmail.com>
2020-11-19 12:23:49 +0700

    update page and add image not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {
+  Button
+} from "react-bootstrap";
 import { notFoundIlustration } from '../../assets/images'
 
 export function NotFound() {
   return (
     <div className="container mt-5">
+      <Button class="btn btn-warning">Go Back</Button>
       <img className="img-fluid" alt="Not found" src={notFoundIlustration} />
     </div>
   )
