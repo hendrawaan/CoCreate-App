0be697ac73ea49e8e1c7154306bc911ace513cb5
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-21 16:51:22 +0800

    Add some pages.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 export * from "./Admin/Admin";
 export * from "./Callback";
+export * from "./Feed/Feed";
 export * from "./Home/Home";
 export * from "./Login/Login";
 export * from "./NotFound/NotFound";
 export * from "./profile/Profile";
 export * from "./Trending/Trending";
-export * from "./PagesAdmin/AdminHome";
 
