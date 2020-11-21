cb0354f56895c1d494361499f0e4b67d4f025b8e
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-21 16:42:17 +0800

    Create profile reducer.

--- src/store/profile.js
+++ src/store/profile.js
@@ -1,123 +1,135 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getUserProfile, updateUserPassword, updateUserProfile, getAnotherUserProfile } from "../api/";
+import {
+  getAnotherUserProfile,
+  getUserProfile,
+  updateUserPassword,
+  updateUserProfile
+} from "../api/";
 
 // Slice
-
 const profileSlice = createSlice({
-    name: "profile",
-    initialState: {},
-    reducers: {
-        onFailed: (state, { payload }) => {
-            state.error = payload;
-            state.loading = false;
-        },
-        getProfileSuccess: (state, { payload }) => {
-            state.error = null;
-            state.loading = false;
-            state.profile = payload;
-        },
-        updateProfileSuccess: (state, { payload }) => {
-            state.error = null;
-            state.loading = false;
-            state.profile = payload;
-        },
-        updatePasswordFailed: (state, { payload }) => {
-            state.error = payload;
-            state.loading = false;
-        },
-        updatePasswordSuccess: (state, { payload }) => {
-            state.profile = payload;
-            state.loading = false;
-        },
+  name: "profile",
+  initialState: {},
+  reducers: {
+    onFailed: (state, { payload }) => {
+      state.error = payload;
+      state.loading = false;
     },
+    getProfileSuccess: (state, { payload }) => {
+      state.error = null;
+      state.loading = false;
+      state.profile = payload;
+    },
+    updateProfileSuccess: (state, { payload }) => {
+      state.error = null;
+      state.loading = false;
+      state.profile = payload;
+    },
+    updatePasswordSuccess: (state, { payload }) => {
+      state.profile = payload;
+      state.loading = false;
+    },
+  },
 });
 
 export default profileSlice.reducer;
 
 const {
-    onFailed,
-    getProfileSuccess,
-    updateProfileSuccess,
-    updatePasswordFailed,
-    updatePasswordSuccess
+  onFailed,
+  getProfileSuccess,
+  updateProfileSuccess,
+  updatePasswordSuccess,
 } = profileSlice.actions;
 
 /**
  * Mengambil profil singkat user.
  * @param {string} token Data token yang akan digunakan untuk Authorization
  */
-export const getProfile = (token) => async(dispatch) => {
-    try {
-        const profileResponse = await getUserProfile(token);
-        switch (profileResponse.code) {
-            case 200:
-                dispatch(getProfileSuccess(profileResponse.data));
-                break;
-            default:
-                throw new Error("Uppss.. Terjadi kesalahan.");
-        }
-    } catch (e) {
-        dispatch(onFailed(e.message));
+export const getProfile = (token) => async (dispatch) => {
+  try {
+    const profileResponse = await getUserProfile(token);
+    switch (profileResponse.code) {
+      case 200:
+        dispatch(getProfileSuccess(profileResponse.data));
+        break;
+      default:
+        throw new Error("Uppss.. Terjadi kesalahan.");
     }
+  } catch (e) {
+    dispatch(onFailed(e.message));
+  }
 };
+
 /**
  * Mengambil profil singkat user lain.
  * @param {string} token Data token yang akan digunakan untuk Authorization
  */
-export const getUserProfileID = (token, id) => async(dispatch) => {
-    try {
-        const profileResponse = await getAnotherUserProfile(token, id);
-        switch (profileResponse.code) {
-            case 200:
-                dispatch(getProfileSuccess(profileResponse.data));
-                break;
-            default:
-                throw new Error("Uppss.. Terjadi kesalahan.");
-        }
-    } catch (e) {
-        dispatch(onFailed(e.message));
+export const getUserProfileID = (token, id) => async (dispatch) => {
+  try {
+    const profileResponse = await getAnotherUserProfile(token, id);
+
+    switch (profileResponse.code) {
+      case 200:
+        dispatch(getProfileSuccess(profileResponse.data));
+        break;
+      default:
+        throw new Error("Uppss.. Terjadi kesalahan.");
     }
+  } catch (e) {
+    dispatch(onFailed(e.message));
+  }
 };
+
 /**
  * Mengupdate Profile.
  * @param {string} token Data token yang akan digunakan untuk Authorization
  * @param {object} data profile yang dikirimkan
  */
-export const updateProfile = ({ name, birth, gender, address, phone, postcode, short_bio }, token) => async(dispatch) => {
+export const updateProfile = ({ name, birth, gender, address, phone, postcode,
+  short_bio }, token) => async (dispatch) => {
     try {
-        const response = await updateUserProfile({ name, birth, gender, address, phone, postcode, short_bio }, token);
-        switch (response.code) {
-            case 200:
-                dispatch(updateProfileSuccess(response.data));
-                break;
-            case 400:
-                throw new Error("Password tidak sesuai.");
-            default:
-                throw new Error("Uppss.. Terjadi kesalahan.");
-        }
+      const response = await updateUserProfile(
+        { name, birth, gender, address, phone, postcode, short_bio },
+        token
+      );
+
+      switch (response.code) {
+        case 200:
+          dispatch(updateProfileSuccess(response.data));
+          break;
+        case 400:
+          throw new Error("Password tidak sesuai.");
+        default:
+          throw new Error("Uppss.. Terjadi kesalahan.");
+      }
     } catch (e) {
-        dispatch(onFailed(e.message));
+      dispatch(onFailed(e.message));
     }
 };
+
 /**
  * Mengupdate Profile.
  * @param {string} token Data token yang akan digunakan untuk Authorization
  * @param {object} data password yang dikirimkan
  */
-export const updatePassword = ({ password_lama, password_baru }, token) => async(dispatch) => {
+export const updatePassword = ({ password_lama, password_baru },
+  token) => async (dispatch) => {
     try {
-        const response = await updateUserPassword({ password_lama, password_baru }, token);
+      const response = await updateUserPassword(
+        { password_lama, password_baru },
+        token
+      );
 
-        switch (response.code) {
-            case 200:
-                dispatch(updatePasswordSuccess(response.data));
-                break;
-            case 400:
-                throw new Error("Password tidak sesuai.");
-            default:
-                throw new Error("Uppss.. Terjadi kesalahan.");
-        }
+      switch (response.code) {
+        case 200:
+          dispatch(updatePasswordSuccess(response.data));
+          break;
+        case 400:
+          throw new Error("Password tidak sesuai.");
+        default:
+          throw new Error("Uppss.. Terjadi kesalahan.");
+      }
     } catch (e) {
-        dispatch(onFailed(e.message));
+      dispatch(onFailed(e.message));
     }
 };
