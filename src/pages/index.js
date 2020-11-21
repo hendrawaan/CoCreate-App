eb7d52e2df121b2e1bdbd47212f455b02bfc2383
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-21 18:05:11 +0800

    Change path.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export * from "./Feed/Feed";
 export * from "./Home/Home";
 export * from "./Login/Login";
 export * from "./NotFound/NotFound";
-export * from "./profile/Profile";
+export * from "./Profile/Profile";
 export * from "./Trending/Trending";
 
