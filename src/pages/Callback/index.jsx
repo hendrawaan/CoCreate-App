12e03c6e7906796b32a5583bcc3df251f70240ac
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-21 16:52:32 +0800

    Redesign view.

--- src/pages/Callback/index.jsx
+++ src/pages/Callback/index.jsx
@@ -1,25 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { loginWithGoogle } from '../../store/user';
-
+import { loginWithGoogle } from "../../store/user";
 
 export const GoogleLoginCallBack = () => {
   const dispatch = useDispatch();
   const { user, error } = useSelector((state) => state.user);
   useEffect(() => {
-    dispatch(loginWithGoogle(window.location.search))
-  }, [dispatch])
+    dispatch(loginWithGoogle(window.location.search));
+  }, [dispatch]);
 
   if(user) {
     return <Redirect to="/" />;
   }
 
-  if(error) {
+  if (error) {
     return <Redirect to="/login" />;
   }
 
   return (
-    <div>Tunggu sebentar sedang mengarahkan Anda.</div>
-  )
-}
+    <Container>
+      <Row className="align-items-center vh-fit">
+        <i className="mx-auto text-center">
+          Harap tunggu, <br /> Kami sedang mengarahkan Anda.
+        </i>
+      </Row>
+    </Container>
+  );
+};
