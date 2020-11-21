f690e9a34b796928bee9ab434d1692c84b50fb2b
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-21 16:42:38 +0800

    Add some reducer.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import admin from './admin';
+import feed from './feed';
 import profile from './profile';
 import user from './user';
 
 const reducer = combineReducers({
+  admin,
   user,
-  profile
+  profile,
+  feed
 })
 
 const store = configureStore({
