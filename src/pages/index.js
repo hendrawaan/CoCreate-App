56887a1b5d4aac1be13af5b0a5ff0fce4b38de73
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-22 16:32:21 +0800

    Add Feed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export * from "./Admin/Admin";
 export * from "./Callback";
 export * from "./DetailPost/DetailPost";
-export * from "./Feed/Feed";
 export * from "./Home/Home";
 export * from "./Login/Login";
 export * from "./NotFound/NotFound";
