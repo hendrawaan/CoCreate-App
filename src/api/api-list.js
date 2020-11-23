2f72508773bb0107395d02d182990f36fa905f28
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-24 03:48:08 +0800

    Add some new Api.

--- src/api/api-list.js
+++ src/api/api-list.js
@@ -48,9 +48,12 @@ export const GET_MY_POST = '/api/v1/user/feeds'
 
 // Get My Category
 export const GET_MY_CATEGORY = '/api/v1/kategori/list/follow'
-    // add Category
+
+// add Category
 export const ADD_MY_CATEGORY = '/api/v1/kategori/set'
-    // Get Another User Category
+
+// Get Another User Category
 export const GET_USER_CATEGORY = '/api/v1/kategori/list/follow/'
-    // Get Another User Feeds
+
+// Get Another User Feeds
 export const GET_USER_FEEDS = '/api/v1/feeds/user/all/'
