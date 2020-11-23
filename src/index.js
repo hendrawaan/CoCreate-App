4092dbcdf4bd587b1c432dea353bba4fd0496231
Abdurraziq Bachmid <a.raziq.1992@gmail.com>
2020-11-24 03:47:45 +0800

    Add style index.css.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
-)
+);
 
 reportWebVitals();
