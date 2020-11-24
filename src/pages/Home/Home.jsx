05856862fa65bde163002d2e117f44b46679db61
zeastank.harits <zeastank.harits@gmail.com>
2020-11-24 07:26:30 +0700

    Fixing bug Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ import {
 } from "react-bootstrap";
 import { homeLogo } from "../../assets/images";
 import { FaHome } from "react-icons/fa";
-import { MdEvent } from "react-icons/md";
+import { MdEvent, MdSettingsInputAntenna } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 import { GrTechnology } from "react-icons/gr";
 import { BiMoney, BiCommentDots } from "react-icons/bi";
@@ -26,6 +26,172 @@ import { logout } from "../../store/user";
 import Add from "./Add";
 import { useHistory } from "react-router-dom";
 
+function Post() {
+  const [realFeeds, setRealFeeds] = useState([]);
+  const { user } = useSelector(state => state.user);
+  const [filter, setFilter] = useState(0);
+  const history = useHistory();
+  const [showcomment, setShowcomment] = useState(-1);
+  const [likes, setLikes] = useState([]);
+  const [clickedcomment, setClickedcomment] = useState();
+
+  const getPost = () => {
+    fetch("http://kelompok6.dtstakelompok1.com/api/v1/feeds/user", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: user.token
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setRealFeeds(data.data.feeds);
+        console.log("realfeeddieffect", data.data.feeds);
+      });
+  };
+
+  useEffect(() => {
+    getPost();
+  }, [likes]);
+
+  useEffect(() => {
+    console.log("like di effect", likes);
+    fetch("http://kelompok6.dtstakelompok1.com/api/v1/feeds/like", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        Authorization: user.token
+      },
+      body: JSON.stringify(likes)
+    }).then(res => res.json());
+  }, [likes]);
+
+  const commentClick = id_post => {
+    console.log("showcomment before:", showcomment);
+    setShowcomment({ showcomment: showcomment === id_post ? -1 : id_post });
+    console.log("di fungsi klik post:", id_post);
+    console.log("di fungsi klik showcomment:", showcomment);
+  };
+
+  const likeClick = id => {
+    setLikes({ id_feed: id, like: true });
+    console.log("ini id", likes);
+  };
+
+  return realFeeds
+    .filter(filtering =>
+      filter === 0 ? filtering.id_kat_feed : filtering.id_kat_feed === filter
+    )
+    .map(items => (
+      <Col md={6} key={items.id}>
+        <Card className="my-4">
+          <Card.Body>
+            <Card.Title>
+              <Row className="d-flex align-items-center justify-content-center">
+                <Col md={2} className="">
+                  <CgProfile size={40} />
+                </Col>
+                <Col md={10} className="">
+                  <p>{items.judul}</p>
+                </Col>
+              </Row>
+              <Row className="d-flex flex-row align-items-center justify-content-center">
+                <p style={{ fontSize: 10 }}>
+                  Posted By: {items.name_user} |{" "}
+                  {moment.unix(items.waktu).format("YYYY-MM-DD HH:mm")} |{" "}
+                  {items.nama_kategori}
+                </p>
+              </Row>
+            </Card.Title>
+            <hr />
+            <Card.Text
+              style={{
+                height: 100,
+                overflow: "hidden",
+                textOverflow: "ellipsis"
+              }}
+            >
+              {items.isi_feed}
+            </Card.Text>
+            <Button
+              variant="outline-primary"
+              onClick={() => history.push("feed/" + items.id)}
+            >
+              Read More
+            </Button>
+          </Card.Body>
+          <Card.Footer>
+            <AiOutlineHeart /> {items.jumlah_liker}
+            {"  "}
+            <BiCommentDots /> {items.jumlah_comment}
+          </Card.Footer>
+          <Card.Footer>
+            <Button
+              variant="danger"
+              // {
+              //   !items.isLikedByUser
+              //     ? "outline-secondary"
+              //     : "danger"
+              // }
+              className="m-1 btn-alert"
+              // onClick={() => {
+              //   setFeeds(prevFeeds =>
+              //     feeds.map(item =>
+              //       item.id_post === items.id_post &&
+              //       items.isLikedByUser == false
+              //         ? {
+              //             ...item,
+              //             liked: item.liked + 1,
+              //             isLikedByUser: true
+              //           }
+              //         : item.id_post === items.id_post &&
+              //           items.isLikedByUser == true
+              //         ? {
+              //             ...item,
+              //             liked: item.liked - 1,
+              //             isLikedByUser: false
+              //           }
+              //         : item
+              //     )
+              //   );
+              // }}
+              onClick={() => setLikes({ id_feed: items.id, like: true })}
+            >
+              <AiOutlineHeart />{" "}
+              {/* {!items.isLikedByUser ? "Like" : "Liked"} */}
+              Like
+            </Button>
+            <Button
+              variant="warning"
+              className="m-1"
+              onClick={() => {
+                commentClick(items.id_post);
+                console.log("dari klik:", items.id);
+              }}
+            >
+              <BiCommentDots /> Comment
+            </Button>
+          </Card.Footer>
+          {showcomment === -1 ? (
+            <Card.Footer className="">
+              <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="basic-addon1">
+                    <CgProfile />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  placeholder="Tulis komentar..."
+                  aria-label="Comment"
+                />
+              </InputGroup>
+            </Card.Footer>
+          ) : null}
+        </Card>
+      </Col>
+    ));
+}
+
 export function Home() {
   const dispatch = useDispatch();
   const { user } = useSelector(state => state.user);
@@ -34,6 +200,7 @@ export function Home() {
 
   const [filter, setFilter] = useState(0);
   const [showcomment, setShowcomment] = useState(-1);
+  const [like, setLike] = useState([]);
   const [clickedcomment, setClickedcomment] = useState();
   const [realFeeds, setRealFeeds] = useState([]);
   const [myCategory, setMyCategory] = useState([{ id: 1 }, { id: 2 }]);
@@ -47,20 +214,31 @@ export function Home() {
 
   let profileData = profile?.user;
 
-  useEffect(() => {
-    fetch("http://kelompok6.dtstakelompok1.com/api/v1/feeds/user", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        Authorization: user.token
-      }
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setRealFeeds(data.data.feeds);
-      });
-  }, [Add]);
+  // useEffect(() => {
+  //   fetch("http://kelompok6.dtstakelompok1.com/api/v1/feeds/user", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //       Authorization: user.token
+  //     }
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       console.log(data);
+  //       setRealFeeds(data.data.feeds);
+  //     });
+  // }, [setLike]);
+
+  // useEffect(() => {
+  //   fetch("http://kelompok6.dtstakelompok1.com/api/v1/feeds/like", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type": "application/json; charset=UTF-8",
+  //       Authorization: user.token
+  //     },
+  //     body: JSON.stringify(like)
+  //   }).then(res => res.json());
+  // }, [like]);
 
   // useEffect(() => {
   //   fetch("http://kelompok6.dtstakelompok1.com/api/v1/kategori/list/follow", {
@@ -78,7 +256,7 @@ export function Home() {
   // }, []);
 
   // console.log("feeds", feeds);
-  console.log("realFeeds", realFeeds);
+  // console.log("realFeeds", realFeeds);
   // console.log("realCategory", myCategory);
 
   const commentClick = id_post => {
@@ -86,6 +264,11 @@ export function Home() {
     setShowcomment({ showcomment: showcomment === id_post ? -1 : id_post });
     console.log("di fungsi klik post:", id_post);
     console.log("di fungsi klik showcomment:", showcomment);
+  };
+
+  const likeClick = id => {
+    setLike({ id_feed: id, like: true });
+    console.log("ini id", like);
   };
 
   return (
@@ -217,121 +400,7 @@ export function Home() {
             <Col style={{}}>
               <Container>
                 <Row className="show-grid">
-                  {realFeeds
-                    .filter(filtering =>
-                      filter === 0
-                        ? filtering.id_kat_feed
-                        : filtering.id_kat_feed === filter
-                    )
-                    .map(items => (
-                      <Col md={6} key={items.id}>
-                        <Card className="my-4">
-                          <Card.Body>
-                            <Card.Title>
-                              <Row className="d-flex align-items-center justify-content-center">
-                                <Col md={2} className="">
-                                  <CgProfile size={40} />
-                                </Col>
-                                <Col md={10} className="">
-                                  <p>{items.judul}</p>
-                                </Col>
-                              </Row>
-                              <Row className="d-flex flex-row align-items-center justify-content-center">
-                                <p style={{ fontSize: 10 }}>
-                                  Posted By: {items.name_user} |{" "}
-                                  {moment
-                                    .unix(items.waktu)
-                                    .format("YYYY-MM-DD HH:mm")}{" "}
-                                  | {items.nama_kategori}
-                                </p>
-                              </Row>
-                            </Card.Title>
-                            <hr />
-                            <Card.Text
-                              style={{
-                                height: 100,
-                                overflow: "hidden",
-                                textOverflow: "ellipsis"
-                              }}
-                            >
-                              {items.isi_feed}
-                            </Card.Text>
-                            <Button
-                              variant="outline-primary"
-                              onClick={() => history.push("feed/" + items.id)}
-                            >
-                              Read More
-                            </Button>
-                          </Card.Body>
-                          <Card.Footer>
-                            <AiOutlineHeart /> {items.jumlah_liker}
-                            {"  "}
-                            <BiCommentDots /> {items.jumlah_comment}
-                          </Card.Footer>
-                          <Card.Footer>
-                            <Button
-                              variant="danger"
-                              // {
-                              //   !items.isLikedByUser
-                              //     ? "outline-secondary"
-                              //     : "danger"
-                              // }
-                              className="m-1 btn-alert"
-                              // onClick={() => {
-                              //   setFeeds(prevFeeds =>
-                              //     feeds.map(item =>
-                              //       item.id_post === items.id_post &&
-                              //       items.isLikedByUser == false
-                              //         ? {
-                              //             ...item,
-                              //             liked: item.liked + 1,
-                              //             isLikedByUser: true
-                              //           }
-                              //         : item.id_post === items.id_post &&
-                              //           items.isLikedByUser == true
-                              //         ? {
-                              //             ...item,
-                              //             liked: item.liked - 1,
-                              //             isLikedByUser: false
-                              //           }
-                              //         : item
-                              //     )
-                              //   );
-                              // }}
-                            >
-                              <AiOutlineHeart />{" "}
-                              {/* {!items.isLikedByUser ? "Like" : "Liked"} */}
-                              Like
-                            </Button>
-                            <Button
-                              variant="warning"
-                              className="m-1"
-                              onClick={() => {
-                                commentClick(items.id_post);
-                                console.log("dari klik:", items.id);
-                              }}
-                            >
-                              <BiCommentDots /> Comment
-                            </Button>
-                          </Card.Footer>
-                          {showcomment === -1 ? (
-                            <Card.Footer className="">
-                              <InputGroup className="mb-3">
-                                <InputGroup.Prepend>
-                                  <InputGroup.Text id="basic-addon1">
-                                    <CgProfile />
-                                  </InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl
-                                  placeholder="Tulis komentar..."
-                                  aria-label="Comment"
-                                />
-                              </InputGroup>
-                            </Card.Footer>
-                          ) : null}
-                        </Card>
-                      </Col>
-                    ))}
+                  <Post />
                 </Row>
               </Container>
             </Col>
